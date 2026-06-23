3b0f626e7b6272ccd73f6cfdece28711610e712d
savidyakolonne <savidya.kolonne@gmail.com>
2026-06-24 00:10:30 +0530

    fix:  validation error log and reg

---
+++ backend/src/main/java/com/skbbank/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.skbbank.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleRuntimeException(RuntimeException ex){
+        return ex.getMessage();
+    }
+}
