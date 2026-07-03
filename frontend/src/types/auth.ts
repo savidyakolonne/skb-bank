e6bc0eadc43fe7dc58ba1e5a28636ec35c7b31ef
savidyakolonne <savidya.kolonne@gmail.com>
2026-07-03 11:20:42 +0530

    feat: FE auth hooks context services folders started

---
+++ frontend/src/types/auth.ts
@@ -0,0 +1,25 @@
+export interface LoginRequest {
+    email: string;
+    password:string;
+}
+
+export interface RegisterRequest {
+    name: string,
+    email: string,
+    password: string
+}
+
+export interface User {
+    id: number,
+    name: string,
+    email: string,
+    role: "USER" | "ADMIN"
+}
+
+export interface AuthResponse {
+    success: boolean
+    message: string,
+    data: {
+        token: string
+    };
+}
